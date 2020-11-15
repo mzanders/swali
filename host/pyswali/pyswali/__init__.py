6289aa98bebbec5baa6268bc7a2a9e0391959517
Maarten Zanders <maarten@zanders.be>
2020-11-15 14:25:56 +0100

    Export Switches in the module.

--- host/pyswali/pyswali/__init__.py
+++ host/pyswali/pyswali/__init__.py
@@ -3,6 +3,7 @@
 from .error import (PyswaliError, RequestError, RequestTimeout)
 from .gateway import Gateway
 from .light import Light
+from .switch import Switch
 
-__all__ = ['Gateway', 'Light', 
+__all__ = ['Gateway', 'Light', 'Switch',
            'PyswaliError', 'RequestError', 'RequestTimeout']
